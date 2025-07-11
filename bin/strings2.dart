3a775e5b9a00590c48f553b182a58f3a854c5e74
Roman-Dewan <mdromandewan1888@gmail.com>
2025-07-11 10:57:56 +0600

    string file 2

---
+++ bin/strings2.dart
@@ -0,0 +1,57 @@
+void main() {
+  /*
+  // upper case & lower case
+  String text1 = "Good Evening, Roman";
+  print("Current text is : $text1");
+  print("uppercase text is : ${text1.toUpperCase()}");
+  print("Lowercase text is : ${text1.toLowerCase()}");
+
+  // trim -> remove the white-spaces from the code, n.b: not in middle
+  String text2 = " Roman Dewan ";
+  print("text2 is :${text2.trim()}:");
+  print("trim-left is :${text2.trimLeft()}:"); // remove left space
+  print("trim-right is :${text2.trimRight()}:"); // remove right space
+
+  // Compare String in Dart
+  String item1 = "Apple";
+  String item2 = "Ant";
+  String item3 = "Basket";
+  String item4 = "Apple";
+
+  print("Comparing item 1 with item 2: ${item1.compareTo(item2)}");
+  print("Comparing item 1 with item 3: ${item1.compareTo(item3)}");
+  print("Comparing item 3 with item 2: ${item3.compareTo(item2)}");
+  print("Comparing item 1 with item 4: ${item1.compareTo(item4)}");
+
+  // Replace
+  String str1 = "I am a good boy I like milk. Doctor says milk is good.";
+  String newText = str1.replaceAll("milk", "water");
+  print("Original text : $str1");
+  print("Replaced Text : $newText");
+
+  // Split
+
+  String text = "apple,banana,mango";
+  List<String> fruits = text.split(","); // splitting by comma
+  print(fruits);
+
+  String sentence = "Dart is fun";
+  List<String> words = sentence.split(" "); // splitting by space
+  print(words);
+
+  // ToString
+  int number = 20;
+  String result = number.toString();
+  print("Type of number is ${number.runtimeType}");
+  print("Type of result is ${result.runtimeType}");
+
+  // SubString
+  String text4 = "I love computer";
+  print("Print only computer : ${text4.substring(7)}");
+  print("Print only love : ${text4.substring(0, 6)}");
+*/
+  String text = "Hello";
+  // print("$text Reverse is ${text.split('').reversed.join()}");
+  print("Reverse is : ${text.split('')}");
+
+}
