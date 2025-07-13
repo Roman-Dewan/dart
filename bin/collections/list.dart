14793ff71c2a989c19f25a4842d09c8a45a8deb5
Roman-Dewan <mdromandewan1888@gmail.com>
2025-07-13 06:13:48 +0600

    dart collections

---
+++ bin/collections/list.dart
@@ -0,0 +1,121 @@
+void main() {
+  /*
+  List<int> ages = [10, 20, 39];
+  print("runtime type of ages : ${ages.runtimeType}");
+  List<String> names = ["roman", "doctor", "faltu"];
+  print("runtime type of names : ${names.runtimeType}");
+  var mixed = [10, "roman", 18.8];
+  print("runtime type of mixed : ${mixed.runtimeType}");
+
+
+  // fixed length list
+
+  // var list = List<int>.filled(5, 0);
+  // print(list);
+
+  // Growable list
+  // var list1 = [210, 21, 22, 33, 44, 55];
+  // print(list1);
+
+  // Access item of list
+  var list1 = [210, 21, 22, 33, 44, 55];
+  print(list1[0]);
+  print(list1[1]);
+  print(list1[2]);
+  print(list1[3]);
+  print(list1[4]);
+  print(list1[5]);
+
+  // Get Index by value
+  // print(list1.indexOf(22));
+  print("index value of 22 is : ${list1.indexOf(22)}");
+
+  // Length of the list
+  print(list1.length);
+
+  // Changing value of list
+  List<String> names = ["Raj", "John", "Rocky"];
+  print(names);
+  names[1] = "Bill";
+  names[2] = "Elon";
+  print(names);
+*/
+
+  /* list properties
+
+      1. first: It returns the first element in the List.
+      2. last: It returns the last element in the List.
+      3. isEmpty: It returns true if the List is empty and false if the List is not empty.
+      4. isNotEmpty: It returns true if the List is not empty and false if the List is empty.
+      5. length: It returns the length of the List.
+      6. reversed: It returns a List in reverse order.
+      7. single: It is used to check if the List has only one element and returns it.
+    
+   */
+  /*     list properties
+  List<String> drinks = ["Water", "Juice", "milk", "coke"];
+  List<int> age = [];
+  List<int> age1 = [23];
+  print("First element of the list is : ${drinks.first}");
+  print("Last element of the list is : ${drinks.last}");
+
+  print("Is Drinks empty: ${drinks.isEmpty.toString()}");
+  print("Is Drinks not empty: ${drinks.isNotEmpty.toString()}");
+
+  print("Is the age is empty : ${age.isEmpty.toString()}");
+  print("Is the age is not empty : ${age.isNotEmpty.toString()}");
+
+  print("Length of drinks: ${drinks.length}");
+  print("List in reverse : ${drinks.reversed}");
+
+  print("single element is : ${age1.single}");
+*/
+  /* 
+  1. add()	Add one element at a time and returns the modified List object.
+  2. addAll()	Insert the multiple values to the given List, and each value is separated by the commas and enclosed with a square bracket ([]).
+  3. insert()	Provides the facility to insert an element at a specified index position.
+  4. insertAll()	Insert the multiple value at the specified index position.
+  
+
+  List<int> evenList = [2, 4, 6];
+  List<int> oddList = [1, 3, 5];
+
+  print(evenList);
+  evenList.add(8);
+  print(evenList);
+  evenList.addAll([10, 12, 14]);
+  print(evenList);
+
+  print(oddList);
+  oddList.insert(1, 7);
+  print(oddList);
+  oddList.insert(3, 9);
+  print(oddList);
+
+  oddList.insertAll(2, [11, 13, 15]);
+  print(oddList);
+  print(oddList.length);
+  oddList.replaceRange(2, 5, [1, 2, 3, 4, 5]);
+  print(oddList);
+  print(oddList.length); 
+ 
+
+  List<int> list = [10, 20, 30, 40, 50, 60];
+  print(list);
+  list.remove(30);
+  print(list);
+
+  list.removeAt(1);
+  print(list);
+
+  list.removeLast();
+  print(list);
+
+  list.addAll([70, 80, 90]);
+  print(list);
+  list.removeRange(2, 5);
+  print(list);
+  */
+
+  
+}
---
+++ bin/collections/map.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+void main() {
+    Map<String, String> countryCapital = {
+      "USA": "Washington",
+      "India": "new delhi",
+      "China": "beijing",
+    };
+    print(countryCapital);
+    // Access values from key
+    print(countryCapital["USA"]);
+    print(countryCapital["China"]); 
+    print(countryCapital["India"]);
+
+    // properties of dart
+
+
+
+}
