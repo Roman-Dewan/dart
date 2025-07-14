26bc232af1bb4c5160cd5566682862e1a0841f49
Roman-Dewan <mdromandewan1888@gmail.com>
2025-07-14 13:53:59 +0600

    adding exam file

---
+++ bin/exam.dart
@@ -0,0 +1,15 @@
+void main() {
+  var list = {1, 2, 3};
+  print(list);
+
+  var map = {'key': 'value'};
+  print(map);
+  // int _myVar = 5;
+  // print(_myVar);
+
+  var numbers = [10, 20, 30];
+  print(numbers[2]);
+
+  // int? Var;
+  // print(Var);
+}
