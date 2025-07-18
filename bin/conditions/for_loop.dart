e078ef213223b736657f96858529011e701d933f
Roman-Dewan <mdromandewan1888@gmail.com>
2025-07-18 14:28:26 +0600

    adding for loop

---
+++ bin/conditions/for_loop.dart
@@ -0,0 +1,30 @@
+void main() {
+  // 1.  Print 1 To 10 Using For Loop
+  // for (int i = 0; i <= 10; i++) {
+  //   print(i);
+  // }
+
+  // 2.  Print 10 To 1 Using For Loop
+  // for (int i = 10; i >= 1; i--) {
+  //   print(i);
+  // }
+
+  // 3.  Print Name 10 Times Using For Loop
+  // for (int i = 0; i < 10; i++) {
+  //   print("roman dewan");
+  // }
+
+  // 4.  Display Sum of n Natural Numbers Using For Loop
+  // int sum = 0;
+  // for (int i = 1; i <= 100; i++) {
+  //   sum += i;
+  // }
+  // print("Sum is : $sum");
+
+  // 5.  Display Even Numbers Between 50 to 100 Using For Loop
+  for (int i = 50; i <= 100; i++) {
+    if (i % 2 == 0) {
+      print(i);
+    }
+  }
+}
