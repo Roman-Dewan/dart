cf842e76b4c2f0f72cef0a20036389cb3a71e9ed
Roman-Dewan <mdromandewan1888@gmail.com>
2025-07-19 00:52:40 +0600

    adding assert file

---
+++ bin/conditions/_1_assert.dart
@@ -0,0 +1,9 @@
+// void main() {
+//   var age = 22;
+//   assert(age == 22);
+// }
+void main() {
+  var age = 13;
+  assert(age >= 18, "Age must be at 18");
+  print("user is $age years old.");
+}
