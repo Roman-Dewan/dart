dc5978afdf69bb703687c7b3047dc67ffabb1599
Roman-Dewan <mdromandewan1888@gmail.com>
2025-07-22 15:06:02 +0600

    adding function 1

---
+++ bin/functions/func1.dart
@@ -0,0 +1,45 @@
+// Example 1: Function That Prints Name
+/*
+void name() {
+  print("My name is Roman");
+}
+
+void main() {
+  name();
+}
+*/
+// Example 2: Function To Find Sum of Two Numbers
+/*
+void sum(var num1, var num2) {
+  print(num1 + num2);
+}
+void main() {
+  sum(5, 2);
+} 
+*/
+
+// Example 3: Function That Find Simple Interest
+/*
+simpleInterest(double principle, double interestRate, double year) {
+  print((principle * interestRate * year) / 100);
+}
+
+main() {
+  double principle = 2000;
+  double interestRate = 10;
+  double year = 1;
+  simpleInterest(principle, interestRate, year);
+}
+*/
+
+// Example 4: Create a function that finds a cube of numbers.
+/*
+void cube(int num) {
+  print("cube of a number is : ${num * num * num}");
+}
+
+void main() {
+  int a = 5;
+  cube(a);
+}
+*/
