4f97e47be3dd278e70b7bf7a5906d323ae348cbf
Roman-Dewan <mdromandewan1888@gmail.com>
2025-07-23 22:36:59 +0600

    ternanry operator added

---
+++ bin/conditions/ternary_operator.dart
@@ -0,0 +1,13 @@
+void main() {
+  var selection = 2;
+  var output = (selection == 2) ? "Apple" : "Banana";
+  print(output);
+
+  var age = 18;
+  var check = (age >= 18) ? "You are a voter" : "You are not a voter.";
+  print(check);
+
+  var age1 = 18;
+  var result = (age1 > 13 && age1 < 19) ? "teenager" : "not teenager";
+  print(result);
+}
